fc66f3b145ae5e2b1f3a20e7f4b25d29e4898c73
Leandro Brás <shadowgaming.7@gmail.com>
2023-03-02 16:22:00 +0000

    Specular Lighting!

--- VulkanTests/src/first_app.cpp
+++ VulkanTests/src/first_app.cpp
@@ -108,6 +108,7 @@ namespace vt
 				GlobalUbo ubo{};
 				ubo.projection = camera.getProjection();
 				ubo.view = camera.getView();
+				ubo.iverseView = camera.getInverseView();
 				pointLightSystem.update(frameInfo, ubo);
 				uboBuffers[frameIndex]->writeToBuffer(&ubo);
 				uboBuffers[frameIndex]->flush();
--- VulkanTests/src/vt_camera.hpp
+++ VulkanTests/src/vt_camera.hpp
@@ -20,11 +20,13 @@ namespace vt
 			glm::vec3 position, glm::vec3 direction, glm::vec3 up = glm::vec3{ 0.f, -1.f, 0.f });
 		void setViewYXZ(glm::vec3 position, glm::vec3 rotation);
 
+		const glm::mat4& getInverseView() const { return inverseViewMatrix; }
 		const glm::mat4& getProjection() const { return projectionMatrix; }
 		const glm::mat4& getView() const { return viewMatrix; }
 
 	private:
 		glm::mat4 projectionMatrix{ 1.f };
 		glm::mat4 viewMatrix{ 1.f };
+		glm::mat4 inverseViewMatrix{ 1.f };
 	};
 }
